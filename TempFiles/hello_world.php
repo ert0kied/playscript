4e4a046011662e5b05bc226a691fedc7451f3a8c
rakibtg <rakibtg@gmail.com>
2015-01-07 19:41:37 +0600

    Released PlayScript

---
+++ TempFiles/hello_world.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+    <style>
+        pre { border: 1px solid #ccc; padding: 10px; border-radius: 5px; }
+        h3 { color: blue; }
+    </style>
+</head>
+<body>
+    <h3>Hello World From &lt;PlayScript/&gt;</h3>
+    <?php
+        $data = array(
+            '* HELLO WORLD!!!',
+            '* THANKYOU FOR USING &lt;PlayScript/&gt;',
+            '* YOUR CONTRIBUTION WILL BE A GREAT GIFT FOR &lt;PlayScript/&gt;',
+            '* LICENSE: OPEN-SOURCE',
+            '* PROJECT HOMEPAGE: playscript.prijm.com',
+            '* YOU CAN RICH ME(INITIAL DEVELOPER) AT twitter.com/rakibtg FEEL FREE TO TWEET OR MESSAGE YOUR QUERY',
+            '* PLEASE SHARE &lt;PlayScript/&gt; IN YOUR SOCIAL MEDIA',
+            '* &lt;PlayScript/&gt; IS AN OPEN-SOURCE BROWSER BASED APP',
+            '* THANKYOU, HAVE A NICE DAY',
+            '* &copy; 2015 prijm.com'
+        );
+        print '<pre>';
+        print_r($data);
+        print '</pre>';
+    ?>
+</body>
+</html>
---
+++ includes/php/footer.php
@@ -0,0 +1,15 @@
+<div class="footer">
+<table width="100%" border="0">
+	<tr>
+		<td width="60%">
+			<?php print $logo; ?> is a browser based tool only used in offline workspaces to instantly code in PHP, PHP based Databases, JavaScript, JavaScript Libraries and also HTML, CSS<br>
+			Really works? Please fork it on <a href="https://github.com/rakibtg/playscript" target="_blank">GitHub</a> by <a href="https://twitter.com/rakibtg" target="_blank">Hasan</a>
+		</td>
+		<td class="footerLogo" width="40%">
+			<span class="logoTop shimmer"><?php print $logo; ?></span>
+		</td>
+	</tr>
+</table>
+</div>
+<script src="includes/js/bootstrap.js"></script>
+<script src="includes/js/playscript.js"></script>
